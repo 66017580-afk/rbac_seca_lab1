ba8c56424c60899d09ed16784d7f875641970bc9
66017580-afk <66017580@rbac.ac.th>
2025-11-04 12:01:11 +0700

    Commit3

--- aboutme.php
+++ aboutme.php
@@ -1,10 +1,14 @@
 <?php
    echo "My name is aj yai";
    echo "I' m a teacher";
-    
+?>
+<?php
+   echo "My name is aj yai";
+   echo "I' m a teacher";
 ?>
 <?php
    echo "My name is aj yai";
    echo "I' m a teacher";
     
 ?>
+
